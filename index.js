1b2de45716b4c2cb43571ddce0b0d7839b0122fc
Piotr Fidler <fidler.piotr@gmail.com>
2019-11-12 09:29:33 +0100

    fix condition

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const formatDate = timeInSeconds => {
+
+  if (!timeInSeconds) {
+    return `0s`;
+  }
+
   let seconds = timeInSeconds % 60;
   let minutes = Math.floor((timeInSeconds / 60) % 60);
   let hours = Math.floor((timeInSeconds / 3600) % 3600);
 
   let time = (hours !== 0 ? `${hours}h ` : ``) + (minutes !== 0 ? `${minutes}m ` : ``) + (seconds !== 0 ? `${seconds}s` : ``);
 
-  if (!timeInSeconds) {
-    return `0s`;
-  }
-return time;
-  
+  return time;
+
 };
 
 module.exports = formatDate;
